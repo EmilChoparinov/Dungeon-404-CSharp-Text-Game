a7db295a7bbcaa7c694cb68f84f7cc4dd4a869d3
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-28 14:17:52 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Dungeon404
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            string[] title = System.IO.File.ReadAllLines("title.txt");
+            foreach(string line in title) Console.WriteLine(line);
+            System.Console.ReadLine();
+        }
+    }
+}
