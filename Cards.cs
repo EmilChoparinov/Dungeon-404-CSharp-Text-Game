3e3e4b8e395450b71ae1eb1cbfb0b975613a7ff1
Connor <connorzimmerman799@yahoo.com>
2017-11-28 17:33:24 -0800

    unfished heroes

--- Cards.cs
+++ Cards.cs
@@ -3,9 +3,59 @@ using System.Collections.Generic;
 
 namespace Cards{
     public class Hero{
+        private string _name;
+        private int _belt_score;
+        private int _hacks;
+        private string[] _art;
+        private int _firewall;
 
-        public Hero(){
-
+        /// <summary>
+        /// Name of hero
+        /// </summary>
+        public string Name{
+            get{
+                return _name;
+            }
+            set{
+                if(value.Length == 0){
+                    throw new ArgumentException("Name field is empty");
+                }
+                else{
+                    _name = value;
+                }
+            }
+        }
+        public int Belt_score{
+            get{
+                return _belt_score;
+            }
+            set{
+                _belt_score = value;
+            }
+        }
+        public int Hacks{
+            get{
+                return _hacks;
+            }
+            set{
+                throw new ArgumentException("Name field is empty");
+            }
+        }
+        public string[] Art{
+            get{
+                return _art;
+            }
+            set{
+                throw new ArgumentException("Name field is empty");
+            }
+        }
+        public int Firewall{
+            get{
+                return _firewall;
+            }
+            set{
+                throw new ArgumentException("Name field is empty");
+            }
         }
     }
 
