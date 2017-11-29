fe4689fc151da3e817c45bef96874390cc26e154
EmilChoparinov <emilchoparinov@gmail.com>
2017-11-28 17:10:19 -0800

    added utils

---
+++ utils.cs
@@ -0,0 +1,14 @@
+namespace Utils
+{
+    class Parser
+    {
+        public static void parseHeros()
+        {
+
+        }
+        public static void parseExtensions()
+        {
+
+        }
+    }
+}
